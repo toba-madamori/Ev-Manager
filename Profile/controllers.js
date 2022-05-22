89003f1f8bc2b34980f1e7706a034abe99825e0a
Toba <tobajamezmadamori@gmail.com>
2022-05-22 19:34:33 +0100

    added validation for update profile

--- Profile/controllers.js
+++ Profile/controllers.js
@@ -2,6 +2,7 @@ const { StatusCodes } = require('http-status-codes')
 const { UnauthenticatedError, BadRequestError, NotFoundError } = require('../Errors')
 const User = require('../Users/models')
 const Profile = require('./models')
+const { profileUpdateValidator } = require('../Utils/validation')
 
 
 const getProfile = async(req,res)=>{
@@ -12,23 +13,23 @@ const getProfile = async(req,res)=>{
 }
 
 const updateProfile = async(req,res)=>{
+    const { error } = profileUpdateValidator.validate({...req.body})
+    if(error) throw new BadRequestError(error.message)
+
     const { name, email, phone_number, tags }= req.body
     const { userID:_id } = req.user
     const { id:profileID } = req.params
     
     const userUpdate = {}
-    if(name && name !== '') userUpdate.name = name
-    if(email && email !== '') userUpdate.email = email
-    if(Object.keys(userUpdate).length !== 0) await User.findByIdAndUpdate({ _id }, userUpdate, {runValidators:true})
+    if(name) userUpdate.name = name
+    if(email) userUpdate.email = email
+    if(Object.keys(userUpdate).length !== 0) await User.findByIdAndUpdate({ _id }, userUpdate)
 
     const profileUpdate = {}
-    if(phone_number && phone_number.length !==0) profileUpdate.phone_number = phone_number
-    if(tags && tags.length !==0){
-        const tagArr= tags.split(',')
-        profileUpdate.tags = tagArr
-    }
+    if(phone_number) profileUpdate.phone_number = phone_number
+    if(tags) profileUpdate.tags = tags
 
-    const updatedProfile = await Profile.findByIdAndUpdate({ _id:profileID}, profileUpdate, { new:true,runValidators:true }).populate('userid', 'name email -_id')
+    const updatedProfile = await Profile.findByIdAndUpdate({ _id:profileID}, profileUpdate, { new:true}).populate('userid', 'name email -_id')
     res.status(StatusCodes.OK).json({ updatedProfile })
 }
 
--- Utils/validation.js
+++ Utils/validation.js
@@ -5,6 +5,9 @@ const name = Joi.string().trim(true).required()
 const email = Joi.string().email().trim(true).required()
 const password = Joi.string().min(6).trim(true).required().strict()
 const provider = Joi.string().required()
+const phone_number = Joi.string().length(11).pattern(/^(?:(?:(?:\+?234(?:\h1)?|01)\h*)?(?:\(\d{3}\)|\d{3})|\d{4})(?:\W*\d{3})?\W*\d{4}$/)
+const tags = Joi.array().items(Joi.string().trim(true))
+const rating = Joi.number().integer().min(1).max(5)
 
 const registerValidator = Joi.object().keys({
     name,
@@ -26,19 +29,17 @@ const changePasswordValidator = Joi.object().keys({
     old_password:password
 })
 
-const validation = Joi.object({
-    userName: Joi.string().alphanum().min(3).max(25).trim(true).required(),
-    email: Joi.string().email().trim(true).required(),
-    password: Joi.string().min(8).trim(true).required(),
-    mobileNumber: Joi.string().length(10).pattern(/[6-9]{1}[0-9]{9}/).required(),
-    birthYear: Joi.number().integer().min(1920).max(2000),
-    skillSet: Joi.array().items(Joi.string().alphanum().trim(true)).default([]),
-    is_active: Joi.boolean().default(true),
-});
+const profileUpdateValidator = Joi.object().keys({
+    name: Joi.string().trim(true),
+    email: Joi.string().email().trim(true),
+    phone_number,
+    tags
+})
 
 module.exports = {
     registerValidator,
     loginValidator,
     forgotPasswordValidator,
-    changePasswordValidator
+    changePasswordValidator,
+    profileUpdateValidator
 }
