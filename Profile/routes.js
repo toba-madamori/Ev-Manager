7f9d0c2388a1ae8fbf5c37a44cd84d3625601a9a
Toba <tobajamezmadamori@gmail.com>
2022-05-25 00:09:21 +0100

    modified profile routes

--- Profile/routes.js
+++ Profile/routes.js
@@ -1,12 +1,12 @@
 const router = require('express').Router()
 const { updateProfile, rateUser, getProfile, getEventHostProfile} = require('./controllers')
 const validator = require('express-joi-validation').createValidator({})
-const { profileUpdateSchema, ratingSchema } = require('../Utils/validation')
+const { profileUpdateSchema, ratingSchema, idValidator } = require('../Utils/validation')
 const authMiddleware = require('../Middleware/authentication')
 
 router.get('/get-profile',authMiddleware, getProfile)
-router.get('/get-host-profile/:id', getEventHostProfile)
-router.patch('/update/:id',authMiddleware, validator.body(profileUpdateSchema), updateProfile)
-router.patch('/rate/:id',validator.query(ratingSchema), rateUser)
+router.get('/get-host-profile/:id',validator.params(idValidator), getEventHostProfile)
+router.patch('/update/:id',authMiddleware, validator.body(profileUpdateSchema),validator.params(idValidator), updateProfile)
+router.patch('/rate/:id',validator.query(ratingSchema),validator.params(idValidator), rateUser)
 
 module.exports = router;
