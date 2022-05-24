5e6d2bd41c55abdd07a5b1e9de182784e7c1cdb3
Toba <tobajamezmadamori@gmail.com>
2022-05-25 00:14:02 +0100

    modified user routes

--- Users/routes.js
+++ Users/routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const passport = require('passport')
 const authentication  = require('../Middleware/authentication')
 const validator = require('express-joi-validation').createValidator({})
-const { registerSchema, loginSchema, forgotPasswordSchema, changePasswordSchema } = require('../Utils/validation')
+const { registerSchema, loginSchema, forgotPasswordSchema, changePasswordSchema,idValidator } = require('../Utils/validation')
 const {
     register,
     login,
@@ -14,10 +14,10 @@ const {
 } = require('./controllers')
 
 router.post('/register',validator.body(registerSchema), register) 
-router.patch('/confirm-registration/:id/:token', confirmRegistration)
+router.patch('/confirm-registration/:id/:token',validator.params(idValidator), confirmRegistration)
 router.post('/login',validator.body(loginSchema), login)
 router.post('/forgot-password',validator.body(forgotPasswordSchema), forgotPassword)
-router.post('/reset-password/:id/:token', resetPassword)
+router.post('/reset-password/:id/:token',validator.params(idValidator), resetPassword)
 router.patch('/change-password',authentication,validator.body(changePasswordSchema),  changePassword) 
 router.get('/auth/google',  passport.authenticate('google', { scope: ['profile','email'] })) // will lead to google Oauth screens
 router.get('/google-redirect-url', passport.authenticate('google'), passportGoogle)
