f99ec8e0e3e9835088b944faab3a0a53300869b4
Toba <tobajamezmadamori@gmail.com>
2022-07-09 09:32:06 +0100

    deployment settings

--- index.js
+++ index.js
@@ -12,11 +12,28 @@ const profileRouter = require('./Profile/routes')
 const eventRouter = require('./Event/routes')
 const restaurantRouter = require('./Restaurant/routes')
 
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
 
 //custom-built middleware
 const notFound = require('./Middleware/notFound')
 const errorHandlerMiddleware = require('./Middleware/errorHandler')
 const authMiddleware = require('./Middleware/authentication')
+
+// extra packages
+app.set('trust proxy', 1)
+app.use(rateLimiter(
+  {
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  }  
+))
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 //inbuilt-middleware
 app.use(express.json())
